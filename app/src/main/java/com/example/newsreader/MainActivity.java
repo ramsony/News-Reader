d8dc49a4356a4b55a97550b9ff4a7492f3ec46a2
Ramson <ramson.yasin@gmail.com>
2019-05-26 10:27:25 -0400

    Setting a class to download data in the background from the web and convert it to Json Format

--- app/src/main/java/com/example/newsreader/MainActivity.java
+++ app/src/main/java/com/example/newsreader/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.newsreader;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,11 +24,77 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
 
-        ListView listView = findViewById( R.id.listView );
+        DownloadTask task = new DownloadTask();
+        try {
+            task.execute( "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty" );
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
+        ListView listView = findViewById( R.id.listView );
         arrayAdapter = new ArrayAdapter( this,android.R.layout.simple_list_item_1,titles );
         listView.setAdapter( arrayAdapter );
+    }
 
 
+
+
+    //Class to Download data from the web in the back ground
+    public class DownloadTask extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try{
+                url = new URL( urls[0] );
+                urlConnection= (HttpURLConnection) url.openConnection();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader( inputStream );
+
+                int data = inputStreamReader.read();
+                while(data != -1){
+                    char current = (char) data;
+                    result += current;
+                    data = inputStreamReader.read();
+                }
+
+                //Changing the data to the JSON format.
+                JSONArray jsonArray = new JSONArray( result );
+                int numberOfItems = 20;
+
+                if(jsonArray.length() < 20){
+                    numberOfItems = jsonArray.length();
+                }
+
+                for (int i=0; i<numberOfItems; i++){
+                    String articleId = jsonArray.getString( i );
+                    url = new URL("https://hacker-news.firebaseio.com/v0/item/" +articleId+ ".json?print=pretty");
+                    urlConnection= (HttpURLConnection) url.openConnection();
+
+                    inputStream = urlConnection.getInputStream();
+                    inputStreamReader = new InputStreamReader( inputStream );
+
+                    data = inputStreamReader.read();
+                    String articleInfo = "";
+                    while(data != -1){
+                        char current = (char) data;
+                        articleInfo += current;
+                        data = inputStreamReader.read();
+                    }
+
+                }
+                Log.i( "URL Content", result );
+                return result;
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }
